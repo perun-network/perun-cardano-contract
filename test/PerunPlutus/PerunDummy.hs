4f0da359df8412f77bdbf1cab0ce15d5081c8fc4
Norbert Dzikowski <norbert@perun.network>
2022-06-01 23:25:08 +0200

    PerunPlutus: Get the project building

---
+++ test/PerunPlutus/PerunDummy.hs
@@ -0,0 +1,13 @@
+module PerunPlutus.PerunDummy where
+
+import Test.Hspec
+  ( Spec,
+    describe,
+    it,
+  )
+import Test.Hspec.Expectations.Pretty
+
+spec :: Spec
+spec =
+  describe "PerunPlutus" $
+    it "does nothing yet" $ 1 `shouldBe` 1
