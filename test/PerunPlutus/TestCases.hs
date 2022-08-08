25b7f54bc9c6e62ef591eece4df5ec9180076ed0
Jan Bormet <jan.bormet@bormet.info>
2022-08-08 13:09:23 +0200

    TestCases: Add test for same-value payout

--- test/PerunPlutus/TestCases.hs
+++ test/PerunPlutus/TestCases.hs
@@ -13,6 +13,7 @@ import Plutus.Contract.Test.ContractModel
 import Test.QuickCheck
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck
+import Test.QuickCheck (Property)
 
 -- Testcases
 
@@ -28,6 +29,18 @@ defaultTimeLockSlots = 15
 
 defaultTimeLock :: Integer
 defaultTimeLock = 15 * 1000
+
+
+-- | sameValuePayoutTest checks that the payout validation works if there are
+-- | multiple outputs with the same value
+sameValuePayoutTest :: (Wallet, Wallet, Wallet) -> DL PerunModel ()
+sameValuePayoutTest (wa, wb, wf) = do
+  channelID <- forAllQ arbitraryQ
+  action $ Open wf [wa, wb] channelID [1_000_000, 1_000_000] defaultTimeLock
+  action Finalize
+  (ChannelState cid _ _ _, _, _, _) <- requireGetChannel "channel must be available after finalization"
+  action $ Close wb [wa, wb] cid
+
 
 -- | honestPaymentTest test scenario:
 -- | a third party opens a channel between A and B,
@@ -223,6 +236,9 @@ aPaysB cs@(ChannelState _ bals v _) delta =
 
 propPerun :: Actions PerunModel -> Property
 propPerun = propRunActions_
+
+prop_SameValuePayoutTest :: Property
+prop_SameValuePayoutTest = withMaxSuccess 1 $ forAllDL (honestPaymentTest (w1, w2, w3)) propPerun
 
 prop_HonestPaymentTest :: Property
 prop_HonestPaymentTest = withMaxSuccess 1 $ forAllDL (honestPaymentTest (w1, w2, w3)) propPerun
