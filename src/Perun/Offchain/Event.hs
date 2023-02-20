df398f641c46331391dcc4bb95a572c55b6f7fc6
Norbert Dzikowski <norbert@perun.network>
2023-02-20 18:24:11 +0100

    Perun/Offchain/Event: Pass ChannelDatum to all required events

--- src/Perun/Offchain/Event.hs
+++ src/Perun/Offchain/Event.hs
@@ -9,17 +9,17 @@ import Perun.Onchain (ChannelDatum (..), version)
 data ChannelEvent
   = -- | Created event, emitted when the channel of interested was first
     -- deployed on-chain.
-    Created
+    Created !ChannelDatum
   | -- | Deposited event, emitting the funding index that deposited the given
     -- values described as a list of balances.
-    Deposited !Integer ![Balance]
+    Deposited !ChannelDatum !Integer ![Balance]
   | -- | Disputed event, emitting the claimed state on-chain.
     Disputed !ChannelDatum
   | -- | Progressed event, indicating the new version number for a channel
     -- state.
-    Progressed !Integer
+    Progressed !ChannelDatum !Integer
   | -- | Concluded event, emitted when a channel is concluded and closed.
-    Concluded
+    Concluded !ChannelDatum
   | -- | Withdrawn event, emitted when a channels funds are redistributed to
     -- its participants.
     Withdrawn
@@ -28,35 +28,3 @@ data ChannelEvent
 -- | Balance is a generic description for balances in Cardano. This can either
 -- be the native currency ADA, or any other Token.
 type Balance = Integer
-
--- | resolveEvent deduces a ChannelEvent. It uses the channels state transition
--- logic between validated ChannelDatums to deduce which event was occurring.
-resolveEvent :: ChannelDatum -> ChannelDatum -> [ChannelEvent]
-resolveEvent
-  oldDatum@( ChannelDatum
-               prevParams
-               prevState
-               prevTime
-               prevFunding
-               prevFunded
-               prevDisputed
-             )
-  newDatum@( ChannelDatum
-               newParams
-               newState
-               newTime
-               newFunding
-               newFunded
-               newDisputed
-             )
-    | lenPrevFunding /= lenNewFunding =
-      [ Deposited
-          (fromIntegral lenPrevFunding)
-          (drop lenPrevFunding newFunding)
-      ]
-    | newDisputed = [Disputed newDatum]
-    | prevState /= newState = [Progressed . version $ newState]
-    | otherwise = []
-    where
-      lenPrevFunding = length prevFunding
-      lenNewFunding = length newFunding
