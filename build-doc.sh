e6aa970232420594dc8b8d742c3d0e61bb19b150
Norbert Dzikowski <norbert@perun.network>
2023-03-30 15:56:39 +0200

    scripts: Use correct executable names

--- build-doc.sh
+++ build-doc.sh
@@ -6,7 +6,7 @@ cabal haddock \
   --haddock-all \
   --haddock-hyperlink-source \
   --enable-documentation \
-  perun-plutus
+  all
 
 echo "Use the above directory to serve a static HTTP server (required if dynamic search with <S> hotkey is wanted)."
 echo "If the quickjump utility is not required simply copy and pasting the above directory to your browser will suffice."
